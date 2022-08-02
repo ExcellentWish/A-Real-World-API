0c7ef71ed262c60c0f9058edd0ce444685eed085
Nigel Doheny <nigel.doheny93@gmail.com>
2022-08-02 12:08:48 +0000

    ADd display exception

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -74,9 +75,12 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
+
+
 
 function displayStatus(data) {
     let heading = "API Key Status";
@@ -84,5 +88,18 @@ function displayStatus(data) {
     results += `<div class="key-status">${data.expiry}</div>`;
     document.getElementById('resultsModalTitle').innerText = heading;
     document.getElementById('results-content').innerHTML = results;
+    resultsModal.show();
+}
+
+function displayException(data) {
+
+    let heading = `<div class="error-heading">An Exception Occurred</div>`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
 }
